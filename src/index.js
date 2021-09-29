bf26c634bc452179e2ea0c75fac82d2c55e13bcf
sina ghaffari <sinaghafari97@gmail.com>
2021-09-29 09:21:20 +0330

    complete project

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import { AppProvider } from "./context";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <AppProvider>
+    <App />
+  </AppProvider>,
+  document.getElementById("root")
+);
